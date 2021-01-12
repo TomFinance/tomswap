6e302c8e550c2d7155cbf9b29effbd6e4cfce816
Derek <derek@r2v.io>
2021-01-12 17:57:55 +0900

    fix: 환경변수 주입 변경

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -7,6 +7,7 @@ const Footer = () => {
         <footer>
             <a href="https://etherscan.io/address/0xF7970499814654CD13Cb7B6E7634A12a7A8A9ABc">Contract</a>
             <a href="https://discord.gg/T7Xgc8f">Discord</a>
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
             <a href="#">Telegram</a>
             <a href="https://twitter.com/defi_tom">Twitter</a>
             <a href="https://github.com/TomFinance">Github</a>
